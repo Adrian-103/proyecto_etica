5458762e2127d087f2377a09b3764465f4ce805a
Adrian-103 <a01665899@tec.mx>
2026-05-31 21:13:32 -0600

    Tested different LED configurations + external power source

--- raspberry/test_leds.py
+++ raspberry/test_leds.py
@@ -3,11 +3,10 @@ import neopixel
 
 pixels = neopixel.NeoPixel(
     board.D18,
-    10,
-    brightness=0.1,
+    160,
+    brightness=1.0,
     auto_write=False
 )
 
-pixels.fill((0, 255, 255))
+pixels.fill((255, 0, 0))
 pixels.show()
-
